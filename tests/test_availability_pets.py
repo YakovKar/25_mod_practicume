b5b63044ef56dc254820d1d202928f0d140551f1
YakovKar <107232570+YakovKar@users.noreply.github.com>
2023-02-28 18:15:34 +0700

    Update test_availability_pets.py

--- tests/test_availability_pets.py
+++ tests/test_availability_pets.py
@@ -5,7 +5,9 @@ from selenium.webdriver.common.by import By
 
 def test_availability_of_all_pets(redirected_to_my_pets):
     """Проверка присутствия всех питомцев"""
-    names = pytest.driver.find_elements(By.XPATH, "//tbody//td[1]")
+    element = WebDriverWait(pytest.driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, ".table.table-hover tbody tr")))
+    pet_data = pytest.driver.find_elements(By.CSS_SELECTOR,".table.table-hover tbody tr")
     #Использую имя питомцев для сравнения со статистикой на сайте
     element = WebDriverWait(pytest.driver, 10).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, ".\\.col-sm-4.left")))
@@ -16,6 +18,6 @@ def test_availability_of_all_pets(redirected_to_my_pets):
     number = number[1].split(":")
     number = int(number[1])
 
-    assert len(names) == number
+    assert len(pet_data) == number
     print("\n" f"Количество питомцев в статистике пользователя: {number}")
-    print(f"Количество питомцев в таблице: {len(names)}")
+    print(f"Количество питомцев в таблице: {len(pet_data)}")
